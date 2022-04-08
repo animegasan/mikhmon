3fbd43b5064b3f3faedff5968b036155b427efa8
Hilman Maulana <hilman0.0maulana@gmail.com>
2022-04-08 15:06:14 +0700

    Create configure.sh

---
+++ src/configure.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Nginx config
+adduser -D -g 'www' www
+chown -R www:www /var/lib/nginx
+mkdir /run/nginx && touch /run/nginx/nginx.pid
+
+# Set variable PHP
+PHP_FPM_USER="www"
+PHP_FPM_GROUP="www"
+PHP_FPM_LISTEN_MODE="0660"
+PHP_MEMORY_LIMIT="512M"
+PHP_MAX_UPLOAD="50M"
+PHP_MAX_FILE_UPLOAD="200"
+PHP_MAX_POST="100M"
+PHP_DISPLAY_ERRORS="On"
+PHP_DISPLAY_STARTUP_ERRORS="On"
+PHP_ERROR_REPORTING="E_COMPILE_ERROR\|E_RECOVERABLE_ERROR\|E_ERROR\|E_CORE_ERROR"
+PHP_CGI_FIX_PATHINFO=0
+
+# Replace some of configuration on www.conf
+sed -i "s|;listen.owner\s*=\s*nobody|listen.owner = ${PHP_FPM_USER}|g" /etc/php7/php-fpm.d/www.conf
+sed -i "s|;listen.group\s*=\s*nobody|listen.group = ${PHP_FPM_GROUP}|g" /etc/php7/php-fpm.d/www.conf
+sed -i "s|;listen.mode\s*=\s*0660|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" /etc/php7/php-fpm.d/www.conf
+sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php7/php-fpm.d/www.conf
+sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php7/php-fpm.d/www.conf
+sed -i "s|;log_level\s*=\s*notice|log_level = notice|g" /etc/php7/php-fpm.d/www.conf
+
+# Replace some of configuration on php.ini
+sed -i "s|display_errors\s*=\s*Off|display_errors = ${PHP_DISPLAY_ERRORS}|i" /etc/php7/php.ini
+sed -i "s|display_startup_errors\s*=\s*Off|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|i" /etc/php7/php.ini
+sed -i "s|error_reporting\s*=\s*E_ALL & ~E_DEPRECATED & ~E_STRICT|error_reporting = ${PHP_ERROR_REPORTING}|i" /etc/php7/php.ini
+sed -i "s|;*memory_limit =.*|memory_limit = ${PHP_MEMORY_LIMIT}|i" /etc/php7/php.ini
+sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /etc/php7/php.ini
+sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php7/php.ini
+sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php7/php.ini
+sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php7/php.ini
